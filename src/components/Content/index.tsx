0878a439d871b7ef227db19e175fb975f13fe335
Bruno Cardoso <brunocpessoal@gmail.com>
2024-04-23 16:33:36 -0300

    feat: add Content component

---
+++ src/components/Content/index.tsx
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+
+import styles from "@/components/Content/styles.module.scss";
+import { ShowsContext } from "@/context/shows";
+
+export function Content() {
+  const { showsGrouped, handleSelectShow } = useContext(ShowsContext);
+
+  return (
+    <div className={styles.container}>
+      {showsGrouped?.map(({ gender, shows }) => (
+        <div className={styles.listContainer}>
+          <h3>{gender}</h3>
+          <div className={styles.list}>
+            {shows.map((show) => (
+              <Link to="/details" className={styles.textLink}>
+                <div
+                  className={styles.card}
+                  onClick={() => handleSelectShow({ show })}
+                >
+                  <img src={show.image.medium} alt="" draggable="false" />
+                  <span>{show.name}</span>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
